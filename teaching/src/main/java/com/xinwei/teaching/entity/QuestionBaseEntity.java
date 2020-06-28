d50d9ffcee6ff35d87ff78a7256d8964897f90eb
程飞 <chengfei@newland.com.cn>
2020-06-28 18:39:28 +0800

    试题模块

---
+++ teaching/src/main/java/com/xinwei/teaching/entity/QuestionBaseEntity.java
@@ -0,0 +1,20 @@
+package com.xinwei.teaching.entity;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @ClassName: QuestionBaseEntity
+ * @Author chengfei
+ * @Date 2020/6/28 18:30
+ * @Version 1.0
+ * @Description: TODO
+ **/
+@Data
+public class QuestionBaseEntity {
+    @ApiModelProperty(value = "试题题干",required = true)
+    private String questionsStart;
+
+    @ApiModelProperty(value = "试题题干对应图片",required = true)
+    private String questionsStartImage;
+}
---
+++ teaching/src/main/java/com/xinwei/teaching/util/MessageConstant.java
@@ -0,0 +1,29 @@
+package com.xinwei.teaching.util;
+
+/**
+ * 消息常量
+ *
+ * @author 顾小伟
+ * @date 2019-05-14
+ */
+public class MessageConstant {
+
+    public static final String ADD_SUCCESS_MESSAGE = "新增成功";
+    public static final String ADD_ERROR_MESSAGE = "新增失败";
+    public static final String UPDATE_SUCCESS_MESSAGE = "修改成功";
+    public static final String UPDATE_ERROR_MESSAGE = "修改失败";
+    public static final String DELETE_SUCCESS_MESSAGE = "删除成功";
+    public static final String DELETE_ERROR_MESSAGE = "删除失败";
+    public static final String QUERY_SUCCESS_MESSAGE = "查询成功";
+    public static final String QUERY_ERROR_MESSAGE = "查询失败";
+    public static final String OPERATION_SUCCESS_MESSAGE = "操作成功";
+    public static final String SYS_ERROR_MESSAGE = "系统错误";
+    public static final String PARAM_ERROR_MESSAGE = "参数异常";
+    public static final String REQUEST_TIME_OUT_ERROR_MESSAGE = "请求超时";
+    public static final String SIGN_ERROR_MESSAGE = "验签失败";
+    public static final String SIGN_ERROR_SUCCESS = "验签成功";
+    public static final String LOGIN_SUCESS = "登录成功";
+    public static final String LOGIN_ERROR = "用户名密码不正确";
+    public static final String DATE_FORMAT_ERROR_MESSAGE = "时间格式错误，无法转换";
+
+}
