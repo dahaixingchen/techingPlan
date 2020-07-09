07858b95d385096f7beddc30e76eb86b3d712dfc
程飞 <chengfei@newland.com.cn>
2020-07-09 10:01:07 +0800

    公共模块修改

--- user/src/main/resources/static/service-worker.js
+++ user/src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fadb04ce445a63216d2dd364d8b1368f.js"
+  "/precache-manifest.a6f946428690959a59047aa84bad40b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
--- user/src/test/java/com/xinwei/teachingplan/test/TestString.java
+++ user/src/test/java/com/xinwei/teachingplan/test/TestString.java
@@ -1,13 +1,19 @@
 package com.xinwei.teachingplan.test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
 import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 public class TestString {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Test
     public void testPlus() {
         String s = "";
@@ -18,6 +24,7 @@ public class TestString {
         long te = System.currentTimeMillis();
         logger.info("+ cost {} ms", te - ts);
     }
+
     @Test
     public void testConcat() {
         String s = "";
@@ -28,6 +35,7 @@ public class TestString {
         long te = System.currentTimeMillis();
         logger.info("concat cost {} ms", te - ts);
     }
+
     @Test
     public void testJoin() {
         List<String> list = new ArrayList<String>();
@@ -39,6 +47,7 @@ public class TestString {
         long te = System.currentTimeMillis();
         logger.info("StringUtils.join cost {} ms", te - ts);
     }
+
     @Test
     public void testStringBuffer() {
         StringBuffer sb = new StringBuffer();
@@ -50,6 +59,7 @@ public class TestString {
         long te = System.currentTimeMillis();
         logger.info("StringBuffer cost {} ms", te - ts);
     }
+
     @Test
     public void testStringBuilder() {
         StringBuilder sb = new StringBuilder();
@@ -65,11 +75,11 @@ public class TestString {
     public static void main(String[] args) throws InterruptedException {
 //        StringBuilder stringBuilder = new StringBuilder();
         final StringBuffer stringBuilder = new StringBuffer();
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    for (int j = 0; j < 1000; j++){
+                    for (int j = 0; j < 1000; j++) {
                         stringBuilder.append("a");
                     }
                 }
@@ -78,5 +88,26 @@ public class TestString {
 
         Thread.sleep(100);
         System.out.println(stringBuilder.length());
+    }
+
+    @Test
+    public void test() {
+        String date_string = "2016-09";
+// 利用java中的SimpleDateFormat类，指定日期格式，注意yyyy,MM大小写
+// 这里的日期格式要求javaAPI中有详细的描述，不清楚的话可以下载相关API查看
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM");
+
+// SimpleDateFormat format=new SimpleDateFormat("yyyyMM");
+// 设置日期转化成功标识
+        boolean dateflag = true;
+// 这里要捕获一下异常信息
+        try {
+            Date date = format.parse(date_string);
+        } catch (ParseException e) {
+            dateflag = false;
+        } finally {
+// 成功：true ;失败:false
+            System.out.println("日期是否满足要求" + dateflag);
+        }
     }
 }
